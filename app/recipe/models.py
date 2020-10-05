d0cb6c6207f211c661812bb4e751fdf7933140c6
João Paludo <jas.paludo@gmail.com>
2020-10-05 20:09:23 -0300

    Add image field to recipe model

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -1,5 +1,15 @@
+import uuid
+import os
 from django.db import models
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    file_name = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', file_name)
 
 
 class Tag(models.Model):
@@ -38,6 +48,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
     tags = models.ManyToManyField(Tag, related_name='recipes')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
--- app/recipe/tests/test_models.py
+++ app/recipe/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from recipe.models import Tag, Ingredient, Recipe
+from unittest.mock import patch
+from recipe import models
 
 
 def sample_user(email='test@gotmail.com', password='testpass', name='Name'):
@@ -10,24 +11,24 @@ def sample_user(email='test@gotmail.com', password='testpass', name='Name'):
 
 def sample_tag(user, name='TestTag'):
     """Create a sample tag"""
-    return Tag.objects.create(user=user, name=name)
+    return mnodels.Tag.objects.create(user=user, name=name)
 
 
 def sample_ingredient(user, name='TestIngredient'):
     """Create a sample ingredient"""
-    return Ingredient.objects.create(user=user, name=name)
+    return mnodels.Ingredient.objects.create(user=user, name=name)
 
 
 class ModelTest(TestCase):
 
     def test_create_retrieve_tag(self):
         """Test creating a tag and retrieving its string representation"""
-        tag = Tag.objects.create(name="TestTag", user=sample_user())
+        tag = models.Tag.objects.create(name="TestTag", user=sample_user())
         self.assertEqual(str(tag), "TestTag")
 
     def test_create_retrieve_ingredient(self):
         """Test ingredient creation and string representation"""
-        ingredient = Ingredient.objects.create(
+        ingredient = models.Ingredient.objects.create(
             name="Test Ingridient",
             user=sample_user()
         )
@@ -35,7 +36,7 @@ class ModelTest(TestCase):
 
     def test_create_recipe(self):
         """Test creating a recipe"""
-        recipe = Recipe.objects.create(
+        recipe = models.Recipe.objects.create(
             user=sample_user(),
             title="Test Recipe",
             time_minutes=5,
@@ -43,3 +44,12 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        filepath = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        self.assertEqual(filepath, f'uploads/recipe/{uuid}.jpg')
